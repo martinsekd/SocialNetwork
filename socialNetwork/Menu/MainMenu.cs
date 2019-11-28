4b92efff5518243cb205231e754622907e8f46f4
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-11-28 11:04:52 +0100

    Menu public wall added

--- socialNetwork/Menu/MainMenu.cs
+++ socialNetwork/Menu/MainMenu.cs
@@ -32,8 +32,9 @@ namespace socialNetwork.Menu
                         Console.WriteLine("(1): Min profil");
                         Console.WriteLine("(2): Venner");
                         Console.WriteLine("(3): Users wall");
-                        Console.WriteLine("(4): Registrer ny bruger");
-                        Console.WriteLine("(5): Exit program");
+                        Console.WriteLine("(4): Public wall");
+                        Console.WriteLine("(5): Registrer ny bruger");
+                        Console.WriteLine("(6): Exit program");
                         Console.WriteLine("---------------------------------");
 
                         string MenuValg = Console.ReadLine();
@@ -67,11 +68,22 @@ namespace socialNetwork.Menu
 
                         //Se user eksistere... ellers break;
 
+                        MenuPublicWall menuPublicWall = new MenuPublicWall(userId);
+                        menuPublicWall.StartMenu();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Skriv min brugers id");
+                        string userId = Console.ReadLine();
+
+                        //Se user eksistere... ellers break;
+
                         MenuUserWall menuUserWall = new MenuUserWall(userId);
                         menuUserWall.StartMenu();
                         break;
 
-                    case "4":
+
+                    case "5":
                         Console.Write("Skriv ny brugers fulde navn: ");
                         string Name = Console.ReadLine();
                         Console.Write("Skriv ny brugers brugernavn: ");
@@ -93,7 +105,7 @@ namespace socialNetwork.Menu
                         }
                         break;
 
-                    case "5":
+                    case "6":
                         Runner = false;
                         break;
 
---
+++ socialNetwork/Menu/MenuPublicWall.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace socialNetwork.Menu
+{
+    class MenuPublicWall
+    {
+        public string MenuValg = "mainMenu";
+
+        public bool Runner = true;
+
+        private string UserId;
+
+        public MenuPublicWall(string userId)
+        {
+            UserId = userId;
+            MenuValg = "mainMenu";
+            Runner = true;
+        }
+
+        public void StartMenu()
+        {
+            while (Runner)
+            {
+                switch (MenuValg)
+                {
+                    case "mainMenu": 
+                        Console.Clear();
+                        Console.WriteLine("Velkommen til Social Network.");
+                        Console.WriteLine("---------------------------------");
+                        Console.WriteLine("Menu: Public wall");
+                        Console.WriteLine("---------------------------------");
+                        Console.WriteLine("(1): Vis seneste post");
+                        Console.WriteLine("(2): Skriv kommentar på post");
+                        Console.WriteLine("(4): Tilbage til main");
+                        Console.WriteLine("---------------------------------");
+
+                        string MenuValg = Console.ReadLine();
+
+                        Console.Clear();
+                        break;
+
+                    case "1":
+                        //Vis seneste post 
+
+                        Console.WriteLine("Tryk enter for at forsætte");
+                        Console.ReadLine();
+                        break;
+
+                    case "2":
+                        Console.Write("Skriv id på Post: ");
+                        string idContent = Console.ReadLine();
+                        Console.Write("Skriv kommentar til Post: ");
+                        string postComment = Console.ReadLine();
+
+                        try
+                        {
+                            // Skriv kommentar på post
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Fejled I at skrive til database");
+                            Console.WriteLine("Tryk enter for at forsætte");
+                            Console.ReadLine();
+                        }
+                        break;
+
+                    case "3":
+                        Runner = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Forkert indtastning");
+                        MenuValg = "mainMenu";
+                        Console.WriteLine("Tryk enter for at forsætte");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
