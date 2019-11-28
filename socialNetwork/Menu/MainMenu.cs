c7a83d5c1afe2243631a290b238c75d8fc72fa45
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-11-28 12:14:08 +0100

    Log on and log off added

--- socialNetwork/Menu/MainMenu.cs
+++ socialNetwork/Menu/MainMenu.cs
@@ -31,13 +31,23 @@ namespace socialNetwork.Menu
                         Console.Clear();
                         Console.WriteLine("Velkommen til Social Network.");
                         Console.WriteLine("---------------------------------");
-                        Console.WriteLine("(1): Min profil");
-                        Console.WriteLine("(2): Venner");
-                        Console.WriteLine("(3): Users wall");
-                        Console.WriteLine("(4): Public wall");
-                        Console.WriteLine("(5): Registrer ny bruger");
-                        Console.WriteLine("(6): Vis alle bruger");
-                        Console.WriteLine("(7): Exit program");
+                        Console.WriteLine("(1): Registrer ny bruger");
+                        Console.WriteLine("(2): Vis alle bruger");
+
+                        if(string.IsNullOrEmpty(userId))
+                        {
+                            Console.WriteLine("(3): Log på");
+                        }
+                        else
+                        {
+                            Console.WriteLine("(4): Log af");
+                            Console.WriteLine("(5): Min profil");
+                            Console.WriteLine("(6): Venner");
+                            Console.WriteLine("(7): Users wall");
+                            Console.WriteLine("(8): Public wall");
+                        }
+                        
+                        Console.WriteLine("(9): Exit program");
                         Console.WriteLine("---------------------------------");
 
                         MenuValg = Console.ReadLine();
@@ -46,35 +56,6 @@ namespace socialNetwork.Menu
                         break;
 
                     case "1":
-                        LogOn();
-                        MenuMyProfil menuMyProfil = new MenuMyProfil(userId);
-                        menuMyProfil.StartMenu();
-                        MenuValg = "mainMenu";
-                        break;
-
-                    case "2":
-                        LogOn();
-                        MenuFriends menuFriends = new MenuFriends(userId);
-                        menuFriends.StartMenu();
-                        MenuValg = "mainMenu";
-                        break;
-
-                    case "3":
-                        LogOn();
-                        MenuPublicWall menuPublicWall = new MenuPublicWall(userId);
-                        menuPublicWall.StartMenu();
-                        MenuValg = "mainMenu";
-                        break;
-
-                    case "4":
-                        LogOn();
-                        MenuUserWall menuUserWall = new MenuUserWall(userId);
-                        menuUserWall.StartMenu();
-                        MenuValg = "mainMenu";
-                        break;
-
-
-                    case "5":
                         Console.Write("Skriv ny brugers fulde navn: ");
                         string Name = Console.ReadLine();
                         Console.Write("Skriv ny brugers brugernavn: ");
@@ -97,13 +78,13 @@ namespace socialNetwork.Menu
                         MenuValg = "mainMenu";
                         break;
 
-                    case "6":
+                    case "2":
                         List<User> user = Db.select.GetAllUsers();
 
                         Console.WriteLine("UserID" + "\t\t\t" + "Nave" + "\t\t\t" + "Alder" + "\t\t\t" + "Køn" + "\n");
                         for (int i = 0; i < user.Count; i++)
                         {
-                            Console.WriteLine( user[i].userid.ToString() + "\t" + user[i].name.ToString() + "\t" + user[i].age.ToString() + "\t" + user[i].gender.ToString());
+                            Console.WriteLine(user[i].userid.ToString() + "\t" + user[i].name.ToString() + "\t" + user[i].age.ToString() + "\t" + user[i].gender.ToString());
                         }
 
                         Console.WriteLine("Tryk enter for at forsætte");
@@ -111,7 +92,42 @@ namespace socialNetwork.Menu
                         MenuValg = "mainMenu";
                         break;
 
+                    case "3":
+                        LogOn();
+                        MenuValg = "mainMenu";
+                        break;
+
+                    case "4":
+                        LogOff();
+                        MenuValg = "mainMenu";
+                        break;
+
+                    case "5":
+                        MenuMyProfil menuMyProfil = new MenuMyProfil(userId);
+                        menuMyProfil.StartMenu();
+                        MenuValg = "mainMenu";
+                        break;
+
+                    case "6":
+                        MenuFriends menuFriends = new MenuFriends(userId);
+                        menuFriends.StartMenu();
+                        MenuValg = "mainMenu";
+                        break;
+
                     case "7":
+                        MenuPublicWall menuPublicWall = new MenuPublicWall(userId);
+                        menuPublicWall.StartMenu();
+                        MenuValg = "mainMenu";
+                        break;
+
+                    case "8":
+                        MenuUserWall menuUserWall = new MenuUserWall(userId);
+                        menuUserWall.StartMenu();
+                        MenuValg = "mainMenu";
+                        break;
+
+
+                    case "9":
                         Runner = false;
                         break;
 
@@ -129,6 +145,13 @@ namespace socialNetwork.Menu
         {
             Console.WriteLine("Skriv min brugers id");
             userId = Console.ReadLine();
+
+            //Se user eksistere... ellers break;
+        }
+
+        public void LogOff()
+        {
+            userId = "";
 
             //Se user eksistere... ellers break;
         }
